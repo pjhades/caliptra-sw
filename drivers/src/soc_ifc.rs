5abf554aaa9205835a0e38a656658089a942bd33
Jing Peng <jipeng@nvidia.com>
2026-09-17 11:16:27 -0400

    wip: drain

--- drivers/src/soc_ifc.rs
+++ drivers/src/soc_ifc.rs
@@ -825,6 +825,7 @@ impl<const LEN: usize> Iterator for StashMeasurementSlotIter<LEN> {
         }
 
         let dword_offset = Self::DWORDS_PER_SLOT * self.current_slot;
+        self.current_slot += 1;
 
         let bytes = match self
             .data
