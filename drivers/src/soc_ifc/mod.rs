74b97bb235dff2e5a2890b1e1d8f90cddd71a1cf
Jing Peng <jipeng@nvidia.com>
2026-09-22 10:21:51 -0400

    wip: reorg stash measurement driver

--- drivers/src/soc_ifc/mod.rs
+++ drivers/src/soc_ifc/mod.rs
@@ -14,13 +14,11 @@ Abstract:
 
 use crate::Array4x12;
 use bitfield::size_of;
-use caliptra_api::mailbox::StashMeasurementData;
 #[cfg(feature = "cfi")]
 use caliptra_cfi_derive::Launder;
 use caliptra_error::{CaliptraError, CaliptraResult};
 use caliptra_registers::soc_ifc::enums::DeviceLifecycleE;
 use caliptra_registers::soc_ifc::{self, SocIfcReg};
-use zerocopy::{FromBytes, IntoBytes};
 
 use crate::{memory_layout, FuseBank};
 
@@ -735,46 +733,6 @@ impl SocIfc {
     pub fn get_clock_period(&self) -> u32 {
         self.soc_ifc.regs().cptra_timer_config().read()
     }
-
-    // XXX this should be gated
-    pub fn stash_measurement_iter(
-        &self,
-    ) -> CaliptraResult<impl Iterator<Item = CaliptraResult<StashMeasurementData>>> {
-        let status = self.soc_ifc.regs().stash_bank_status().read();
-        let end_stash = status.end_stash();
-        let slot_locked = status.slot_locked();
-
-        // SoC asserted end-of-stash without locking any slot, or
-        // SoC populated and locked slots but failed to assert end-of-stash.
-        if end_stash && slot_locked == 0 || !end_stash && slot_locked != 0 {
-            return Err(CaliptraError::RUNTIME_STASH_MEASUREMENT_BANK_INVALID_STATUS);
-        }
-
-        // Check if measurements are populated sequentially.
-        if slot_locked.trailing_ones() + slot_locked.leading_zeros() != u32::BITS {
-            return Err(CaliptraError::RUNTIME_STASH_MEASUREMENT_BANK_INVALID_STATUS);
-        }
-
-        Ok(StashMeasurementSlotIter {
-            data: self.soc_ifc.regs().stash_bank_slot_data().read(),
-            current_slot: 0,
-            num_slots: slot_locked.trailing_ones() as usize,
-        })
-    }
-
-    // XXX this should be gated
-    pub fn lock_stash_measurement_bank(&mut self) {
-        self.soc_ifc
-            .regs_mut()
-            .stash_bank_cptra_lock()
-            .write(|w| w.cptra_lock(true));
-    }
-
-    // XXX this should be gated
-    pub fn poll_end_stash(&self) -> CaliptraResult<()> {
-        while !self.soc_ifc.regs().stash_bank_status().read().end_stash() {}
-        Ok(())
-    }
 }
 
 bitfield::bitfield! {
@@ -821,39 +779,5 @@ pub enum ResetReason {
     Unknown,
 }
 
-// XXX these should be gated
-
-pub struct StashMeasurementSlotIter<const LEN: usize> {
-    data: [u32; LEN],
-    current_slot: usize,
-    num_slots: usize,
-}
-
-impl<const LEN: usize> StashMeasurementSlotIter<LEN> {
-    const DWORDS_PER_SLOT: usize = core::mem::size_of::<StashMeasurementData>() / 4;
-}
-
-impl<const LEN: usize> Iterator for StashMeasurementSlotIter<LEN> {
-    type Item = CaliptraResult<StashMeasurementData>;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.current_slot >= self.num_slots {
-            return None;
-        }
-
-        let dword_offset = Self::DWORDS_PER_SLOT * self.current_slot;
-        self.current_slot += 1;
-
-        let result = self
-            .data
-            .get(dword_offset..dword_offset + Self::DWORDS_PER_SLOT)
-            .map(|dwords| dwords.as_bytes())
-            .ok_or(CaliptraError::RUNTIME_STASH_MEASUREMENT_SLOT_OUT_OF_BOUNDS)
-            .and_then(|bytes| {
-                StashMeasurementData::read_from_bytes(&bytes)
-                    .map_err(|_| CaliptraError::RUNTIME_STASH_MEASUREMENT_SLOT_SIZE_ERROR)
-            });
-
-        Some(result)
-    }
-}
+#[cfg(hw_rev = "2.2")]
+pub mod stash_measurement;
---
+++ drivers/src/soc_ifc/stash_measurement.rs
@@ -0,0 +1,92 @@
+/*++
+
+Licensed under the Apache-2.0 license.
+
+File Name:
+
+    stash_measurement.rs
+
+Abstract:
+
+    File contains APIs for accessing stash measurment registers via SoC interface
+
+--*/
+
+use crate::soc_ifc::SocIfc;
+use caliptra_api::mailbox::StashMeasurementData;
+use caliptra_error::{CaliptraError, CaliptraResult};
+use zerocopy::{FromBytes, IntoBytes};
+
+impl SocIfc {
+    pub fn stash_measurement_iter(
+        &self,
+    ) -> CaliptraResult<impl Iterator<Item = CaliptraResult<StashMeasurementData>>> {
+        let status = self.soc_ifc.regs().stash_bank_status().read();
+        let end_stash = status.end_stash();
+        let slot_locked = status.slot_locked();
+
+        // SoC asserted end-of-stash without locking any slot, or
+        // SoC populated and locked slots but failed to assert end-of-stash.
+        if end_stash && slot_locked == 0 || !end_stash && slot_locked != 0 {
+            return Err(CaliptraError::RUNTIME_STASH_MEASUREMENT_BANK_INVALID_STATUS);
+        }
+
+        // Check if measurements are populated sequentially.
+        if slot_locked.trailing_ones() + slot_locked.leading_zeros() != u32::BITS {
+            return Err(CaliptraError::RUNTIME_STASH_MEASUREMENT_BANK_INVALID_STATUS);
+        }
+
+        Ok(StashMeasurementSlotIter {
+            data: self.soc_ifc.regs().stash_bank_slot_data().read(),
+            current_slot: 0,
+            num_slots: slot_locked.trailing_ones() as usize,
+        })
+    }
+
+    pub fn lock_stash_measurement_bank(&mut self) {
+        self.soc_ifc
+            .regs_mut()
+            .stash_bank_cptra_lock()
+            .write(|w| w.cptra_lock(true));
+    }
+
+    pub fn poll_end_stash(&self) -> CaliptraResult<()> {
+        while !self.soc_ifc.regs().stash_bank_status().read().end_stash() {}
+        Ok(())
+    }
+}
+
+pub struct StashMeasurementSlotIter<const LEN: usize> {
+    data: [u32; LEN],
+    current_slot: usize,
+    num_slots: usize,
+}
+
+impl<const LEN: usize> StashMeasurementSlotIter<LEN> {
+    const DWORDS_PER_SLOT: usize = core::mem::size_of::<StashMeasurementData>() / 4;
+}
+
+impl<const LEN: usize> Iterator for StashMeasurementSlotIter<LEN> {
+    type Item = CaliptraResult<StashMeasurementData>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.current_slot >= self.num_slots {
+            return None;
+        }
+
+        let dword_offset = Self::DWORDS_PER_SLOT * self.current_slot;
+        self.current_slot += 1;
+
+        let result = self
+            .data
+            .get(dword_offset..dword_offset + Self::DWORDS_PER_SLOT)
+            .map(|dwords| dwords.as_bytes())
+            .ok_or(CaliptraError::RUNTIME_STASH_MEASUREMENT_SLOT_OUT_OF_BOUNDS)
+            .and_then(|bytes| {
+                StashMeasurementData::read_from_bytes(&bytes)
+                    .map_err(|_| CaliptraError::RUNTIME_STASH_MEASUREMENT_SLOT_SIZE_ERROR)
+            });
+
+        Some(result)
+    }
+}
